ab833ac61a37c44d95914d9a2b9ccce6b13fd9be
mapmapbear <37301000+mapmapbear@users.noreply.github.com>
2026-06-11 21:56:12 +0800

    style(app): convert main() indentation to tabs
    Align MinimalLatestApp.cpp with the tab indentation used across render/
    and rhi/; whitespace-only change.

--- app/MinimalLatestApp.cpp
+++ app/MinimalLatestApp.cpp
@@ -6,28 +6,28 @@
 
 int main()
 {
-    // getchar();
-  // Get the logger instance
-  utils::Logger& logger = utils::Logger::getInstance();
-  // logger.enableFileOutput(false);  // Don't write log to file
-  logger.setShowFlags(utils::Logger::eSHOW_TIME);
-  logger.setLogLevel(utils::Logger::LogLevel::eINFO);  // Default is Warning, we show more information
-  LOGI("Starting ... ");
+	// getchar();
+	// Get the logger instance
+	utils::Logger& logger = utils::Logger::getInstance();
+	// logger.enableFileOutput(false);  // Don't write log to file
+	logger.setShowFlags(utils::Logger::eSHOW_TIME);
+	logger.setLogLevel(utils::Logger::LogLevel::eINFO); // Default is Warning, we show more information
+	LOGI("Starting ... ");
 
-  try
-  {
-    ASSERT(glfwInit() == GLFW_TRUE, "Could not initialize GLFW!");
-    ASSERT(glfwVulkanSupported() == GLFW_TRUE, "GLFW: Vulkan not supported!");
+	try
+	{
+		ASSERT(glfwInit() == GLFW_TRUE, "Could not initialize GLFW!");
+		ASSERT(glfwVulkanSupported() == GLFW_TRUE, "GLFW: Vulkan not supported!");
 
-    MinimalLatestApp app({800, 600});
-    app.run();
+		MinimalLatestApp app({800, 600});
+		app.run();
 
-    glfwTerminate();
-  }
-  catch(const std::exception& e)
-  {
-    LOGE("%s", e.what());
-    return 1;
-  }
-  return 0;
+		glfwTerminate();
+	}
+	catch (const std::exception& e)
+	{
+		LOGE("%s", e.what());
+		return 1;
+	}
+	return 0;
 }
